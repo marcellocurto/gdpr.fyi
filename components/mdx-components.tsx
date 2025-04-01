d6cc47f1a7b7d856f4e470462b5dd71973816955
Marcello <marcello@roark.at>
2025-04-01 16:21:50 +0200

    static export images

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,4 +1,5 @@
-import Image, { ImageProps } from "next/image";
+import Image from "next-export-optimize-images/image";
+import { ImageProps } from "next/image";
 
 export const MDXComponents = {
 	Image: (props: ImageProps) => <Image {...props} />,
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 import remarkGfm from "remark-gfm";
+import withExportImages from "next-export-optimize-images";
+
 const nextConfig: NextConfig = {
 	output: "export",
 	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
@@ -12,4 +14,4 @@ const withMDX = createMDX({
 	},
 });
 
-export default withMDX(nextConfig);
+export default withExportImages(withMDX(nextConfig));
